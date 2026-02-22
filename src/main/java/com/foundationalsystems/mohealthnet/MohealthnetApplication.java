ab2498561b4e4e6c7716bcb7c6fdb2588b62ef59
Dajoun1 <dajoun99@gmail.com>
2026-02-22 13:10:37 -0600

    Setup Spring Boot with a controller and tests

---
+++ src/main/java/com/foundationalsystems/mohealthnet/MohealthnetApplication.java
@@ -0,0 +1,13 @@
+package com.foundationalsystems.mohealthnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohealthnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohealthnetApplication.class, args);
+	}
+
+}
