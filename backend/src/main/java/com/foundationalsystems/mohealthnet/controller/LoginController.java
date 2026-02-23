9a66ccc984041bc377c90a04cfa3384db0ced344
Dajoun1 <dajoun99@gmail.com>
2026-02-23 13:44:35 -0600

    Added /login endpoint for demo

--- backend/src/main/java/com/foundationalsystems/mohealthnet/controller/LoginController.java
+++ backend/src/main/java/com/foundationalsystems/mohealthnet/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.foundationalsystems.mohealthnet.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,14 @@ import java.util.Map;
 public class LoginController {
 
     private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
+
+// Test endpoint
+    @GetMapping("/login")
+    public String login() {
+        LOG.info("GET /login endpoint called");
+        return "Testing login endpoint";
+    }
+
 
     /**
      * Handles POST requests to /auth/login for user authentication.
